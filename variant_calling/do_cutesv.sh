ae740576c3d05a888c1aad1c1df83f7e1b312204
Henry <k001y@odcf-worker01.inet.dkfz-heidelberg.de>
2020-11-06 11:49:11 +0100

    update

--- variant_calling/do_cutesv.sh
+++ variant_calling/do_cutesv.sh
@@ -4,7 +4,7 @@
 #BSUB -W 8:00
 #BSUB -R "rusage[mem=20GB]"
 #BSUB -e error_file%J
-#BSUB -J cutesv_test
+#BSUB -J Ovise_varcalling
 
 module load anaconda3/2019.07
 
@@ -12,7 +12,7 @@ source activate cutesv-env
 
 cuteSV /icgc/dkfzlsdf/analysis/C010/brooks/master_pipeline/output/Ovise.sorted.bam \
 /icgc/dkfzlsdf/analysis/C010/brooks/GCF_000001405.26_GRCh38_genomic.fna \
-/icgc/dkfzlsdf/analysis/C010/brooks/variant_calling/output/test.vcf \
-/icgc/dkfzlsdf/analysis/C010/brooks/variant_calling \
---threads 4
+/icgc/dkfzlsdf/analysis/C010/brooks/master_pipeline/output/Ovise.vcf \
+/icgc/dkfzlsdf/analysis/C010/brooks/master_pipeline \
+--sample Ovise --threads 4 --max_cluster_bias_INS 100 --diff_ratio_merging_INS 0.3 --max_cluster_bias_DEL 100 --diff_ratio_merging_DEL 0.3
 
