390963a2c474e4f4eee8e242348907bc573dcd4d
Henry <k001y@odcf-worker01.inet.dkfz-heidelberg.de>
2020-11-10 11:02:53 +0100

    update

--- del_detect/scripts/bam_slicer.sh
+++ del_detect/scripts/bam_slicer.sh
@@ -2,9 +2,9 @@
 
 module load samtools/1.9
 
-INP_DIR=/icgc/dkfzlsdf/analysis/C010/mayerma/methylation_analysis/mapping
+INP_DIR=/icgc/dkfzlsdf/analysis/C010/brooks/master_pipeline/output
 
-OUT_DIR=/icgc/dkfzlsdf/analysis/C010/brooks/del_detect/bam_slicing
+OUT_DIR=/icgc/dkfzlsdf/analysis/C010/brooks/downstream/ashish
 
 bams=`ls -1 $INP_DIR/*.bam`
 
@@ -13,6 +13,6 @@ bams=`ls -1 $INP_DIR/*.bam`
 for bf in $bams
 do
 bn=`basename $bf`
-samtools view -h -b $bf 17:64803882-66303882 > $OUT_DIR/del_chr17_64803882_66303882_${bn}
-samtools index $OUT_DIR/del_chr17_64803882_66303882_${bn}
+samtools view -h -b $bf NC_000003.12:167000000-170000000 > $OUT_DIR/chr3_roi${bn}
+samtools index $OUT_DIR/chr3_roi${bn}
 done
