f23d57de30118f254a925b52487d5fe5395ec3da
Henry <k001y@odcf-worker01.inet.dkfz-heidelberg.de>
2020-11-23 14:08:07 +0100

    update

--- assembly_pipeline/scripts/do_guppy_gpu.sh
+++ assembly_pipeline/scripts/do_guppy_gpu.sh
@@ -5,42 +5,42 @@
 #BSUB -W 48:00
 #BSUB -n 10
 #BSUB -e error_file%J
-#BSUB -J Ovise_hac_basecalling
+#BSUB -J AR_hac_basecalling
 
 module load cudnn/7.6.1.34/cuda10.0
 
-DATASET_LOCATION=/datasets/datasets_k001y/Ovise/fast5_files/
-EXPERIMENT_LOCATION=/datasets/datasets_k001y/Ovise/basecalled/   
+#DATASET_LOCATION=/datasets/datasets_k001y/Ovise/fast5_files/
+#EXPERIMENT_LOCATION=/datasets/datasets_k001y/Ovise/basecalled/   
 
-tmp_dir=/ssd/k001y/${LSB_JOBID}
-echo job is running at $tmp_dir
+#tmp_dir=/ssd/k001y/${LSB_JOBID}
+#echo job is running at $tmp_dir
 
-tmp_dir_data=${tmp_dir}/data # this is where I will store my dataset
-mkdir $tmp_dir_data
-tmp_dir_exp=${tmp_dir}/exp # here results are stored
-mkdir $tmp_dir_exp
+#tmp_dir_data=${tmp_dir}/data # this is where I will store my dataset
+#mkdir $tmp_dir_data
+#tmp_dir_exp=${tmp_dir}/exp # here results are stored
+#mkdir $tmp_dir_exp
 
-rsync -rtu ${DATASET_LOCATION}/ ${tmp_dir_data} # this copies the data to the SSD
-rsync -rtu ${EXPERIMENT_LOCATION}/ ${tmp_dir_exp} # this copies (previous) experiment results to the SSD
+#rsync -rtu ${DATASET_LOCATION}/ ${tmp_dir_data} # this copies the data to the SSD
+#rsync -rtu ${EXPERIMENT_LOCATION}/ ${tmp_dir_exp} # this copies (previous) experiment results to the SSD
 
-DATASET_LOCATION_old=$DATASET_LOCATION
-DATASET_LOCATION=$tmp_dir_data
-export DATASET_LOCATION
-EXPERIMENT_LOCATION_old=$EXPERIMENT_LOCATION
-EXPERIMENT_LOCATION=$tmp_dir_exp
-export EXPERIMENT_LOCATION
+#DATASET_LOCATION_old=$DATASET_LOCATION
+#DATASET_LOCATION=$tmp_dir_data
+#export DATASET_LOCATION
+#EXPERIMENT_LOCATION_old=$EXPERIMENT_LOCATION
+#EXPERIMENT_LOCATION=$tmp_dir_exp
+#export EXPERIMENT_LOCATION
 
-guppy_basecaller --input_path $DATASET_LOCATION \
---save_path $EXPERIMENT_LOCATION \
+guppy_basecaller --input_path /gpu/data/OE0287/k001y/nanopore_raw/fast5/ \
+--save_path /gpu/checkpoints/OE0287/k001y/nanopore_basecalled/ \
 -c /home/k001y/programs/ont-guppy-gpu/data/dna_r9.4.1_450bps_hac.cfg \
 --num_callers 10 \
 -x auto \
 --recursive
 
-rsync -rtu ${EXPERIMENT_LOCATION}/ ${EXPERIMENT_LOCATION_old}
+#rsync -rtu ${EXPERIMENT_LOCATION}/ ${EXPERIMENT_LOCATION_old}
 
 # revert DATASET_LOCATION. Not sure if really needed
-DATASET_LOCATION=$DATASET_LOCATION_old
-export DATASET_LOCATION
-EXPERIMENT_LOCATION=$EXPERIMENT_LOCATION_old
-export EXPERIMENT_LOCATION
+#DATASET_LOCATION=$DATASET_LOCATION_old
+#export DATASET_LOCATION
+#EXPERIMENT_LOCATION=$EXPERIMENT_LOCATION_old
+#export EXPERIMENT_LOCATION
